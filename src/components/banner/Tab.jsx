58aa9a413c4c862e97f1190422edd2746a9de414
Emon-Ahmed <technicalboyemon@gmail.com>
2025-03-09 17:57:22 +0600

    fix: updated favicon icon

--- src/components/banner/Tab.jsx
+++ src/components/banner/Tab.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 
-export default function Tab({ text }) {
+export default function Tab({ text, active, setActive }) {
   return (
-    <div className="inline-block mx-1 px-4 py-2 hover:bg-[#444BFF] hover:text-white rounded cursor-pointer">
+    <div
+      className={`inline-block mx-1 px-4 py-2 ${
+        active ? "bg-[#444BFF] text-white" : "bg-white text-black"
+      } rounded cursor-pointer`}
+      onClick={() => setActive(!active)}
+    >
       {text}
     </div>
   );
--- src/components/banner/Tabs.jsx
+++ src/components/banner/Tabs.jsx
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Tab from "./Tab";
 
 export default function Tabs() {
+  const [active, setActive] = useState(true);
   return (
     <div className="p-2 border-2 border-[#111111]/8 inline-block items-center justify-center rounded ">
-      <div className="px-4 py-2 bg-[#444BFF] text-white rounded inline-block cursor-pointer">
+      <div
+        className={`px-4 py-2 ${
+          active ? "bg-[#444BFF] text-white" : "bg-[#FFF] text-black"
+        } rounded inline-block cursor-pointer`}
+        onClick={() => setActive(!active)}
+      >
         View all
       </div>
-      <Tab text="Crypto" />
-      <Tab text="Blockchain" />
-      <Tab text="Announcements" />
-      <Tab text="People" />
-      <Tab text="Engineering" />
-      <Tab text="NFT" />
+      <Tab text="Crypto" active={active} setActive={setActive} />
+      <Tab text="Blockchain" active={active} setActive={setActive} />
+      <Tab text="Announcements" active={active} setActive={setActive} />
+      <Tab text="People" active={active} setActive={setActive} />
+      <Tab text="Engineering" active={active} setActive={setActive} />
+      <Tab text="NFT" active={active} setActive={setActive} />
     </div>
   );
 }
